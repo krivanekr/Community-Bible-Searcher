08fe1190f9e8d0d37f3e016c1485503bcd5c9184
krivanekr <krivanekr@rsd407.org>
2023-06-12 10:31:40 -0700

    Finished Project 2

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
         NewTestament,
         OldTestament;
     }
-    public static void main(String args[]) {
+    public static void main(String args[]) throws FileNotFoundException {
 
         boolean running = true;
         do {
-            System.out.println("Do you want to:\nA - Search for verses with a word or phrase\nB - Associate a word with a verse\nC - Search for a specific verse");
+            System.out.println("Do you want to:\nA - Search for verses for a word or phrase\nB - Associate a word with a verse\nC - Search for a specific verse\nD - Get random verse");
             Scanner s = new Scanner(System.in);
             String task = s.next();
             if (task.equalsIgnoreCase("A")) {
@@ -61,11 +61,11 @@ public class Main {
             }
             else if (task.equalsIgnoreCase("B")){
                 ArrayList<String> myArray = new ArrayList<>();
-                System.out.println("What do you want to add to this program?");
                 Scanner r = new Scanner(System.in);
-                String word = r.nextLine();
-                System.out.println("What verse do you want to add it to?");
+                System.out.println("What verse do you want to associate a word with?");
                 String verse = r.nextLine();
+                System.out.println("What do you want to add to this verse?");
+                String word = r.nextLine();
                 boolean found = false;
                 try {
                     Scanner s2 = new Scanner(new File("KeyWords.txt"));
@@ -105,14 +105,31 @@ public class Main {
                     Scanner s2 = new Scanner(new File("BibleWithBookNames.txt"));
                     while (s2.hasNextLine()) {
                         String line = s2.nextLine();
-                        if (line.indexOf(verse) != -1) {
+                        if (line.indexOf(verse + " ") != -1) {
                             System.out.println(line);
                         }
                     }
                 } catch (FileNotFoundException e) {
                     System.out.println("Error Reading File; " + e);
                 }
-            } else {
+            }
+            else if (task.equalsIgnoreCase("D")){
+                Random rand = new Random();
+                int verseLineNumber = rand.nextInt(31102);
+                try {
+                    Scanner s3 = new Scanner(new File("BibleWithBookNames.txt"));
+                    for (int i = 0; i < verseLineNumber; i++) {
+                        if (s3.hasNextLine()) {
+                            s3.nextLine();
+                        }
+                    }
+                    System.out.println(s3.nextLine());
+                    System.out.println();
+                }catch (FileNotFoundException e) {
+                    System.out.println("Error Reading File; " + e);
+                }
+            }
+            else {
                 System.out.println("Invalid input...");
             }
         System.out.println("\nType \"c\" to continue.\nType anything else to exit.");
@@ -123,3 +140,21 @@ public class Main {
         }while (running);
     }
 }
+
+
+//Search for just verses associated orf just verses with word
+//Groups of verses instead of just single verses (+3 from starting verse implies starting verse and next 3)
+//Similar words (friend, friendship)
+//Plural (friend, friends)
+//Check for proper formatting (to avoid corrupt KeyWords.txt file)
+//Remove words
+//Be able to associate word with verse searched
+//Be able to associate a word with verse in list of verses that come up
+//Show verse before associating word
+//Give random verse                                                                                             DONE
+//Give random word list from KeyWords.txt
+//Have choice to select wrong verse
+//Make words not case sensitive or require proper format
+//Say if verse doesn't exist
+//Show format for verses
+//Search for groups of verses
